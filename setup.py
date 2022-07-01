d12c8a275b5926b40f444fc97a4b11226f420a07
phonsoswag <95614205+phonsoswag@users.noreply.github.com>
2022-07-01 14:44:03 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='metaplex-python-api',
+version='0.9.4',
+description='Testing installation of Package',
+url='#',
+author='auth',
+author_email='author@email.com',
+license='MIT',
+packages=['metaplex-python-api'],
+zip_safe=False)
